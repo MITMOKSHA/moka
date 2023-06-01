6352011bfb2af2d5ab1de2917d55d9f929bb58e5
moksha <moksha0224@berkeley.edu>
2023-06-01 12:02:55 +0800

    增加关键注释，因为hook针对的是IO协程调度器，因此在普通调度器中不能使用hook版本的API，因为并没有对TimerManager进行初始化

--- tests/test_scheduler.cc
+++ tests/test_scheduler.cc
@@ -8,7 +8,8 @@ moka::Logger::ptr g_logger = MOKA_LOG_ROOT();
 void test_fiber() {
   MOKA_LOG_INFO(g_logger) << "test in fiber and counts=" << moka::Fiber::GetFiberCounts();
   static int s_count = 3;
-  // 这一块hook之后就不支持嵌套加锁了
+  // hook之后普通的协程调度就不能使用该版本了，因为在IOManager的时候才对父类TimerManager进行初始化
+  // sleep中会调用addTimer，但是此时TimerManager对象并未初始化，因此就会出现segFault
   // sleep(1);
   if (--s_count >= 0) {
     // moka::Scheduler::GetThis()->schedule(test_fiber, moka::GetThreadId());  // 指定线程执行
@@ -17,7 +18,7 @@ void test_fiber() {
 }
 
 void test_scheduler() {
-  moka::Scheduler s(2, true, "test");
+  moka::Scheduler s(1, true, "test");
   s.start();
   // 添加函数作为任务执行
   moka::Fiber::ptr task(new moka::Fiber(test_fiber));
